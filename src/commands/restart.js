edf3ff1b449a98b4c42571b8d037ade62d67ffd4
MrFiNka <48646488+MrFiNka@users.noreply.github.com>
2024-09-05 20:06:15 +0300

    restart

--- src/commands/restart.js
+++ src/commands/restart.js
@@ -1,29 +1,46 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+/*
+	Copyright (C) 2022 Alexander Emanuelsson (alexemanuelol) and FaiThiX
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+	https://github.com/alexemanuelol/rustplusplus
+
+*/
+
+const Builder = require('@discordjs/builders');
+
+const DiscordMessages = require('../discordTools/discordMessages.js');
 
 module.exports = {
 	name: 'restart',
 
 	getData(client, guildId) {
-		return new SlashCommandBuilder()
+		return new Builder.SlashCommandBuilder()
 			.setName('restart')
-			.setDescription('Перезапускает процесс Node.js');
+			.setDescription(client.intlGet(guildId, 'restartingProcess'));
 	},
 
 	async execute(client, interaction) {
-		const guildId = interaction.guildId;
-
 		const verifyId = Math.floor(100000 + Math.random() * 900000);
 		client.logInteraction(interaction, verifyId, 'slashCommand');
 
 		if (!await client.validatePermissions(interaction)) return;
-		await interaction.deferReply({ ephemeral: true });
 
-		const str = client.intlGet(guildId, 'restartingProcess');
-		await client.interactionEditReply(interaction, {
-			embeds: [client.discordEmbeds.getActionInfoEmbed(0, str)]
-		});
 		client.log(client.intlGet(null, 'infoCap'), str);
 		rustplus.log(client.intlGet(null, 'infoCap'), str);
+
+		await DiscordMessages.sendInfoMessage(interaction);
 
 		// Завершение процесса Node.js
 		process.exit(0);
