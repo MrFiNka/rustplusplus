7cdb56f7dc36fe89ca9634f8624b5595832c34c8
MrFiNka <48646488+MrFiNka@users.noreply.github.com>
2025-03-07 20:56:15 +0300

    this._queuedPlayers

--- src/structures/Info.js
+++ src/structures/Info.js
@@ -31,7 +31,7 @@ class Info {
         this._wipeTime = info.wipeTime;
         this._players = info.players;
         this._maxPlayers = info.maxPlayers;
-        this._queuedPlayers = 0;
+      //  this._queuedPlayers = info.queuedPlayers || 0;
         this._seed = info.seed;
         this._salt = info.salt;
         
@@ -96,7 +96,7 @@ class Info {
         this.wipeTime = info.wipeTime;
         this.players = info.players;
         this.maxPlayers = info.maxPlayers;
-        this.queuedPlayers = info.queuedPlayers;
+      //  this.queuedPlayers = info.queuedPlayers;
         this.seed = info.seed;
         this.salt = info.salt;
 
