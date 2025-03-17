0e13e14b7799837436dd33ae1f5822521a795ad2
MrFiNka <48646488+MrFiNka@users.noreply.github.com>
2025-03-18 00:42:04 +0300

    switchPairingMessage

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ echo ---------------------- START (%DATE:~0,2%.%DATE:~3,2% %TIME:~0,8%) --------
 #git checkout -- package-lock.json
 git pull
 #export NPM_CONFIG_YES=true
-npm install --legacy-peer-deps --force --yes
+#npm install --legacy-peer-deps --force --yes
 node .
 rm -vrf logs
 rm -vrf maps
